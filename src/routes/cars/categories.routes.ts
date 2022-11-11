9bf6dfe95656e4e57e99de41a1f2a225fe5dec43
Natanael Araújo <natanael@hotmail.co.jp>
2022-11-11 04:35:58 -0300

    Upload de arquivos

--- src/routes/cars/categories.routes.ts
+++ src/routes/cars/categories.routes.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { createCategoryController } from '../../modules/cars/useCases/category/createCategory';
+import { importCategoryController } from '../../modules/cars/useCases/category/importCategory';
 import { listCategoriesController } from '../../modules/cars/useCases/category/listCategory';
 
 const carsCategoriesRoutes = Router();
+
+const upload = multer({
+  dest: './tmp',
+});
 
 carsCategoriesRoutes.post('/', (request, response) => {
   return createCategoryController.handle(request, response);
@@ -12,5 +18,16 @@ carsCategoriesRoutes.post('/', (request, response) => {
 carsCategoriesRoutes.get('/', (request, response) => {
   return listCategoriesController.handle(request, response);
 });
+
+carsCategoriesRoutes.post(
+  '/import',
+  upload.single('file'),
+  (request, response) => {
+    return importCategoryController.handle(
+      request,
+      response,
+    )
+  },
+)
 
 export { carsCategoriesRoutes };
