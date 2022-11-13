1f496ba73e647449ae021f5ea7036c4b145338b6
Natanael Araújo <natanael@hotmail.co.jp>
2022-11-12 23:04:10 -0300

    swagger configurado

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 import express from 'express'
+import swaggerUi from 'swagger-ui-express'
+
+import swaggerFile from './swagger.json';
 
 import { router } from './routes';
 
@@ -7,6 +10,7 @@ const port = 3333;
 
 app.use(express.json());
 app.use(router)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.listen(port, () => {
   console.log(`[SERVER RUNNING]: listening on port: ${port}`);
