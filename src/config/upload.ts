c18e1ceb413e7492a730c5f35f377e65d2962f84
Natanael Araújo <natanael@hotmail.co.jp>
2022-12-05 19:45:48 -0300

    fix: multer generating undefined folder in root

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -3,10 +3,15 @@ import multer from "multer";
 import { resolve } from "path";
 
 export default {
-  upload(folder?: string) {
+  upload(folder: string) {
     return {
       storage: multer.diskStorage({
-        destination: resolve(__dirname, "..", "..", `${folder}`),
+        destination: resolve(
+          __dirname,
+          "..",
+          "..",
+          `${folder !== undefined ? folder : ""}`
+        ),
         filename: (request, file, callback) => {
           const fileHash = crypto.randomBytes(16).toString("hex");
           const fileName = `${fileHash}-${file.originalname}`;
